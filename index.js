f01bae17ad7ab295af9f7684abdce275e799cc31
maruf-hussain <engmarufhussain@gmail.com>
2023-09-27 00:16:28 +0600

    fifgdfa

--- index.js
+++ index.js
@@ -66,6 +66,13 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/bookings/:id', async(req, res)=>{
+            const id = req.params.id
+            const query = {_id: new ObjectId(id)}
+            const result = await bookingsCollection.deleteOne(query)
+            res.send(result)
+        })
+
       
         
         // Send a ping to confirm a successful connection
